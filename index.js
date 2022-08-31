6acb231d5024723c67a116fce8560b5fa20ac47d
joserivera <joserivera0015@gmail.com>
2022-08-30 23:26:08 -0500

    agregado index.js para servidor node

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const fs = require('fs')
+const port = 5000
+const server = http.createServer((req, res) => {
+	res.writeHead(200, {'Content-Type': 'text/html'})
+	fs.readFile('index.html', (error, data) => {
+		res.write(data)
+		res.end()
+	})
+})
+server.listen(port, () => console.log('Funcionando'))
