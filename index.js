c0d2e345074712e7cab0e6f73c203d12d8a4d002
joserivera <joserivera0015@gmail.com>
2022-08-31 00:06:24 -0500

    agregado console.log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const port = 5000
 const server = http.createServer((req, res) => {
+	console.log(req.url)
 	res.writeHead(200, {'Content-Type': 'text/html'})
 	fs.readFile('index.html', (error, data) => {
 		res.write(data)
